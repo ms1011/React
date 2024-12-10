f544b45920bdf67ecc48ec08d0b199641a339071
DESKTOP-L6SHIAA\소프트퍼즐 <ms1011@naver.com>
2024-12-10 14:41:12 +0900

    리스트 렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import './App.css'
 import Todo from './components/Todo'
 import Avatar from './components/Avatar'
 import PackingList from './components/PackingList'
+import PeopleList from './components/List'
 
 function App() {
 
   return (
     <div>
+      <h2>People List</h2>
+      <PeopleList />
       <h2>Packing List</h2>
       <PackingList />
       <h2>Todo List</h2>
