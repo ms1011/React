b03794f743de5f04578807399bd58c086cb487ca
DESKTOP-L6SHIAA\소프트퍼즐 <ms1011@naver.com>
2024-12-10 12:42:51 +0900

    조건부 랜더링

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
-// import './App.css'
+import './App.css'
 import Todo from './components/Todo'
 import Avatar from './components/Avatar'
+import PackingList from './components/PackingList'
 
 function App() {
 
   return (
     <div>
+      <h2>Packing List</h2>
+      <PackingList />
       <h2>Todo List</h2>
       <Todo />
       <h2>Avatar</h2>
---
+++ src/components/PackingList.jsx
@@ -0,0 +1,35 @@
+function Item({ name, isPacked }) {
+    // 기본적인 조건부 랜더링
+    // if (isPacked) {
+    //     return <li className="item">{name} ✅</li>;
+    // }
+    // return <li className="item">{name}</li>;
+    return (
+        <li className="item">
+            {/* {isPacked ? name + ' ✅' : name} 삼항 연산자 이용 */}
+            {name} {isPacked && '✅'}
+        </li>
+    );
+}
+
+export default function PackingList() {
+    return (
+        <section>
+            <h1>패킹 리스트</h1>
+            <ul>
+                <Item
+                    isPacked={true}
+                    name={"등산복"}
+                />
+                <Item
+                    isPacked={true}
+                    name={"등산화"}
+                />
+                <Item
+                    isPacked={false}
+                    name={"등산스틱"}
+                />
+            </ul>
+        </section>
+    );
+}
