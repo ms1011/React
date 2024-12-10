6b06c2ce1061ab5776f7398e9487d0fb1f578248
DESKTOP-L6SHIAA\소프트퍼즐 <ms1011@naver.com>
2024-12-10 15:46:09 +0900

    이벤트 핸들러 넣기

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,17 @@ import Todo from './components/Todo'
 import Avatar from './components/Avatar'
 import PackingList from './components/PackingList'
 import PeopleList from './components/List'
+import Toolbar from './components/Toolbar'
 
 function App() {
 
   return (
     <div>
+      <h2>Toolbar</h2>
+      <Toolbar 
+        onPlayMovie={() => alert('Playing!')}
+        onUploadImage={() => alert('Uploading!')}
+      />
       <h2>People List</h2>
       <PeopleList />
       <h2>Packing List</h2>
