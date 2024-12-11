e592eb88ea8faff47bab16385da6c32d38812b60
DESKTOP-L6SHIAA\소프트퍼즐 <ms1011@naver.com>
2024-12-11 09:33:28 +0900

    router 설정 + 객체 state 업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Todo from './components/Todo'
 import Avatar from './components/Avatar'
@@ -8,32 +9,48 @@ import Toolbar from './components/Toolbar'
 import Gallery from './components/Gallery'
 import Form from './components/Form'
 import Counter from './components/Counter'
+import MovingDot from './components/MovingDot'
+
+function Home() {
+    return (
+      <div>
+        <h2>Counter</h2>
+        <Counter />
+        <h2>Form</h2>
+        <Form />
+        <h2>Gallery</h2>
+        <Gallery />
+        <Gallery />
+        <h2>Toolbar</h2>
+        <Toolbar 
+          onPlayMovie={() => alert('Playing!')}
+          onUploadImage={() => alert('Uploading!')}
+        />
+        <h2>People List</h2>
+        <PeopleList />
+        <h2>Packing List</h2>
+        <PackingList />
+        <h2>Todo List</h2>
+        <Todo />
+        <h2>Avatar</h2>
+        <Avatar />
+      </div>
+    )
+}
+
+function DotPage() {
+  return <MovingDot />;
+}
 
 function App() {
 
   return (
-    <div>
-      <h2>Counter</h2>
-      <Counter />
-      <h2>Form</h2>
-      <Form />
-      <h2>Gallery</h2>
-      <Gallery />
-      <Gallery />
-      <h2>Toolbar</h2>
-      <Toolbar 
-        onPlayMovie={() => alert('Playing!')}
-        onUploadImage={() => alert('Uploading!')}
-      />
-      <h2>People List</h2>
-      <PeopleList />
-      <h2>Packing List</h2>
-      <PackingList />
-      <h2>Todo List</h2>
-      <Todo />
-      <h2>Avatar</h2>
-      <Avatar />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/dot' element={<DotPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
