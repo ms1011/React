271b4784e774a48a4c89893184d6de81c5098a65
DESKTOP-L6SHIAA\소프트퍼즐 <ms1011@naver.com>
2024-12-12 09:47:59 +0900

    배열에 immer 사용가능 주석 추가

--- src/components/ListChange.jsx
+++ src/components/ListChange.jsx
@@ -10,6 +10,7 @@ export default function ListChange() {
      * react 에서는 push()를 사용하여 배열을 변경하지 않는다.
      * 객체와 마찬가지로 React state에서 배열은 읽기 전용으로 처리해야 한다. push()나 pop() 같은 함수로 배열을 변경해서는 안된다.
      * 대신 배열을 업데이트할 때마다 새 배열을 state 설정 함수에 전달해야한다.
+     * immer를 사용하는 것도 가능하다.
      */
     return (
         <>
